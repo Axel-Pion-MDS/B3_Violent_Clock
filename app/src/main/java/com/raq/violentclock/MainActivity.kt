f5130936fc1414ee7a1c3259c4e55ddedb14afbb
Rémi Rubis <remi.rubis@yahoo.com>
2022-04-07 00:20:46 +0200

    feat(new-page): Add new page for alarm creation

--- app/src/main/java/com/raq/violentclock/MainActivity.kt
+++ app/src/main/java/com/raq/violentclock/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.raq.violentclock
 
-// For navigate
-//import android.content.Intent
-//val intent = Intent(this, PlayerActivity::class.java)
-
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -91,6 +89,9 @@ class MainActivity : AppCompatActivity() {
         var btnAlarm : FloatingActionButton = findViewById<FloatingActionButton>(R.id.addAlarm)
         btnAlarm.setOnClickListener {
             Log.d("MainActivityDebug", "Click!")
+            val intent : Intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         }
     }
 }
