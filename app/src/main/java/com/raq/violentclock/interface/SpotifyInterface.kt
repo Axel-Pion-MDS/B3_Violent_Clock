5cf6cf5335128359b14bf07f17c70bfa1e2c0105
Axel Pion <axel.pion@betc.com>
2022-04-07 10:21:07 +0200

    correct conflict

--- app/src/main/java/com/raq/violentclock/interface/SpotifyInterface.kt
+++ app/src/main/java/com/raq/violentclock/interface/SpotifyInterface.kt
@@ -6,11 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
-<<<<<<< Updated upstream
-private const val TOKEN = "Bearer BQDJ28Jqp1gruEHW9dprr9z27TAl7lPSO_28srAjsrD_fxy7CLLaczMz3eOsbN244ZngrDLyuwBsnAapi-NA5lKaDi-K-onWmZtixkLxebbOGdtLaE-c2yqdNunknMkLAiHbJ0cu-X-HxpRBnj4"
-=======
 private const val TOKEN = "Bearer BQDpsdVDCakpFeBqz4x3RCjkrQjtuZkPwfZPvR32UGNTTMxIQnC9qIoEEpiM7xUsNKPaET8AcHIA-oZ61qOXDRi0T0pRhTW7n1tK8QgHdfSxpZGH4ueYtJFE9sSGFPN6chKteOD3cnDjf_s"
->>>>>>> Stashed changes
 private const val SEARCH_TYPE = "track"
 
 interface SpotifyInterface {
